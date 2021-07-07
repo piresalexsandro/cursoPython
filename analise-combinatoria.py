8b0017807c3cb6a778848a35b9ef62d3a02f773c
piresalexsandro <allexsandro.pires@gmail.com>
2021-07-07 09:03:23 -0300

    analise combinatoria ajuste

--- analise-combinatoria.py
+++ analise-combinatoria.py
@@ -1,9 +1,4 @@
-# Arranjos
-# Nos arranjos, os agrupamentos dos elementos dependem da ordem e da natureza dos mesmos.
-# Para obter o arranjo simples de n elementos tomados, p a p (p ≤ n), utiliza-se a seguinte expressão:
-# A com n vírgula p subscrito fim do subscrito igual a numerador n fatorial sobre denominador parêntese
-# esquerdo n menos p parêntese direito fatorial fim da fração
-
+# Assim, para calcular uma combinação simples de n elementos tomados p a p (p ≤ n), utiliza-se a seguinte expressão:
 from math import factorial
 
 e = int(input("Digite o numero de elementos: "))
@@ -12,8 +7,8 @@ p = int(input("Digite o numero de combinacoes entre si: "))
 def getFactorial(num):
     return factorial(num)
 
-def getCombinations(elem, poss):
-    return getFactorial(elem) / getFactorial(elem - poss)
+def getCombinations(elem, poss): #Cn,p = n! / (p!*(n - p)!)
+    return getFactorial(elem) / (getFactorial(poss) * getFactorial(elem - poss))
 
 print("O total de combinacoes de {} elementos tomados {} a {} e de: {}".format(e, p, p, getCombinations(e, p)))
 

b39f5d8199ab96873e8d261a2f35bd88229f37dc
piresalexsandro <allexsandro.pires@gmail.com>
2021-09-11 23:01:23 -0300

    estrutura de controle

---
+++ stdr-3307-laco-repeticao.py
@@ -0,0 +1,14 @@
+num = 0
+
+while(num <= 100):
+    num=num+1
+    if num <= 100:
+        print(num)
+
+num = 0
+while(num <= 20):
+    num=num+1
+    if num <= 20:
+        print(num)
+else:
+    print('chegamos ao fim')    
---
+++ stdr-3308-laco-interacao.py
@@ -0,0 +1,5 @@
+clientes = ['Julia', 'Pedro', 'Ana']
+for cliente in clientes:
+    print(cliente)
+else:
+    print('todos os clientes foram listados')    

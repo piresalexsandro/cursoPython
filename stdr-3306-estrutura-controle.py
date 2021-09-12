ab9e87507aa93803c765826752a66bbafd867bbb
piresalexsandro <allexsandro.pires@gmail.com>
2021-09-11 22:42:26 -0300

    funcoes

---
+++ stdr-3306-estrutura-controle.py
@@ -0,0 +1,9 @@
+nacionalidade = input("Digite a nacionalidade: ")
+
+if nacionalidade == 'e':
+    print('Cliente estrangeiro')
+elif nacionalidade == 'b':
+    print('Cliente brasileiro')  
+else:
+    print('nacionalidade nao encontrada')  
+      
